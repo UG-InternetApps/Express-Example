b8187de358ced7e3cab93a7b3d178da2258482a9
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-10-25 09:39:07 -0500

    Add html file response

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/index.html', function(req, res) {
+  res.sendFile(__dirname + '/' + 'index.html');
+});
+
 app.get('/', function(req, res) {
   res.send('Hola MAMA estoy haciendo express');
 });
